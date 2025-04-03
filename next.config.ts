55eb5f70eb9fdda9713f3566429b581233024b66
Pedro Botega <botegapedro@gmail.com>
2025-04-03 19:08:21 +0100

    creates netlify.tml and updates next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Isso é essencial para gerar arquivos estáticos
+  distDir: 'out',   // Especifica a pasta de saída (Netlify espera isso)
+  images: {
+    unoptimized: true, // Necessário quando usando 'output: export'
+  },
+  // Outras configurações específicas do seu projeto podem vir aqui
 };
 
 export default nextConfig;
